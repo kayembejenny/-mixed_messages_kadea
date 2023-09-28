3a389bf899b54e92cdd330365a88e8c76f0f1a93
christian birego <93344052+birego@users.noreply.github.com>
2023-09-28 10:52:27 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const message = ["BONJOUR", "SALUT", "BAY BAY", "COUCOU CHRIS", "J'AIME golden States warrior"];
+
+const random = Math.floor(Math.random() * message.length);
+console.log(message[random]);
